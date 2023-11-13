0fea7ef6428394826fdff404304e26bda64333b0
Hans <ianolpx@gmail.com>
2023-11-13 23:21:03 +0800

    test: test04

--- pnzer/__init__.py
+++ pnzer/__init__.py
@@ -1,10 +1,3 @@
 
-def normalizer():
-    print("normalizer")
-
-class NP:
-    def __init__(self):
-        pass
-
-    def run(self):
-        print("NP.run")
+def normalize():
+    print("normalize() called")
